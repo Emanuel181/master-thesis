cb2522d323aa2da4db7305942a4cee7cb1bc1d97
Emanuel Rusu <emanuel.rusu03@e-uvt.ro>
2025-12-06 16:48:53 +0200

    m

--- components/dashboard/sidebar/team-switcher.jsx
+++ components/dashboard/sidebar/team-switcher.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import Image from "next/image"
 import {
     SidebarMenu,
     SidebarMenuButton,
@@ -22,7 +23,7 @@ export function TeamSwitcher({
                     size="lg"
                 >
                     <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                        <team.logo className="size-4" />
+                        <Image src={team.logo} width={16} height={16} alt="" className="size-4" />
                     </div>
                     <div className="grid flex-1 text-left text-sm leading-tight">
                         <span className="truncate font-medium">{team.name}</span>
